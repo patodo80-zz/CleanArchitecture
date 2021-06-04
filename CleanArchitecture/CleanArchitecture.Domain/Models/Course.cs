507eb6ae66558c6cefcc743cc3028a354427b429
Patrick Odoherty <Patrick.Odoherty@hyland.com>
2021-06-04 15:57:43 -0400

    Add Course Domain Model

---
+++ CleanArchitecture/CleanArchitecture.Domain/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Models
+{
+    class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set;  }
+        public string Description { get; set;  }
+        public string ImageURL { get; set; }
+
+    }
+}
