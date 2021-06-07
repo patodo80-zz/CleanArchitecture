75c28c3a67283fc7624e0176c65bb73907995be2
Patrick Odoherty <Patrick.Odoherty@hyland.com>
2021-06-07 14:21:17 -0400

    Initial Commit of Infrastructure.Data Project, Update MVC for new UniversityDBContext

--- CleanArchitecture/CleanArchitecture.Domain/Models/Course.cs
+++ CleanArchitecture/CleanArchitecture.Domain/Models/Course.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CleanArchitecture.Domain.Models
 {
-    class Course
+    public class Course
     {
         public int ID { get; set; }
         public string Name { get; set;  }
---
+++ CleanArchitecture/CleanArchitecture.Infra.Data/Migrations/20210607171108_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Infra.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
