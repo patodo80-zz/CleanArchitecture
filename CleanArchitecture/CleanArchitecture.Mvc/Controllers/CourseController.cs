3a67fa081d038c69baaebb7aee9ce00a00ca5226
Patrick Odoherty <Patrick.Odoherty@hyland.com>
2021-06-08 16:40:16 -0400

    Commit Changes for Presentation Course Controller, Views, Styling, Authentication

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
