c5282d8f79324e1a8b4ce265dab84731d73a8e2d
Patrick Odoherty <Patrick.Odoherty@hyland.com>
2021-06-09 08:35:49 -0400

    Adding Domain.Core and Implementation of InMemeoryBus

---
+++ CleanArchitecture/CleanArchitecture.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
