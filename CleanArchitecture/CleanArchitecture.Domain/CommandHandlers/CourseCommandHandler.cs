d3bf5e40e725fb4aa24c8e61b02474d9438009c7
Patrick Odoherty <Patrick.Odoherty@hyland.com>
2021-06-09 14:03:35 -0400

    Commands, CommandHandlers, Updated Course Repository, Cleaned Up IOC Container

---
+++ CleanArchitecture/CleanArchitecture.Domain/CommandHandlers/CourseCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchitecture.Domain.Commands;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.CommandHandlers
+{
+    public class CourseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseCommandHandler(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public Task<bool> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = new Course()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageURL = request.ImageURL
+            };
+
+            _courseRepository.Add(course);
+
+            return Task.FromResult(true);
+        }
+    }
+}
--- CleanArchitecture/CleanArchitecture.Infra.IoC/DependencyContainer.cs
+++ CleanArchitecture/CleanArchitecture.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.CommandHandlers;
+using CleanArchitecture.Domain.Commands;
+using CleanArchitecture.Domain.Core.Bus;
 using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infra.Bus;
+using CleanArchitecture.Infra.Data.Context;
 using CleanArchitecture.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +19,19 @@ namespace CleanArchitecture.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Infra.Data layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+
+            services.AddScoped<UniversityDBContext>();
         }
     }
 }
